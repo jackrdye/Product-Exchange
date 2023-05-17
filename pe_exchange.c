7320315f6a2a66977b5398844261ef33eda9cfd1
= <=>
2023-05-17 22:27:27 +1000

    Should compile

--- pe_exchange.c
+++ pe_exchange.c
@@ -87,6 +87,8 @@ void cleanup_traders() {
 Trader** create_traders(int num_traders, char **argv) {
     traders = (Trader**) malloc((num_traders) * sizeof(Trader*));
     for (int i = 0; i < num_traders; i++) {
+        Trader* new_trader = malloc(sizeof(Trader*));
+        traders[i] = new_trader;
         traders[i]->id = i; // Set trader id
         char buf[MAX_FIFO_LENGTH];
         int num_bytes;
